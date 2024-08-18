7be668ee71a4644d429959776c16ed9eedc14e09
Sahil Kumar <sagramanisahil@gmail.com>
2024-08-18 14:12:27 +0500

    Add Ugly Number

---
+++ UglyNumber.java
@@ -0,0 +1,10 @@
+class UglyNumber {
+    public boolean isUgly(int n) {
+       for(int i=2; i<6 && n >0; i++){
+        while(n % i == 0){
+            n /= i;
+        }
+       }
+       return n == 1;
+    }
+}
