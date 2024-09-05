da68a292cee3736a6182865b2517ab0150f1b607
Sahil Kumar <sagramanisahil@gmail.com>
2024-09-05 15:17:12 +0500

    Add OneBitTwoBit

---
+++ OneBitTwoBit.java
@@ -0,0 +1,9 @@
+class OneBitTwoBit {
+    public boolean isOneBitCharacter(int[] bits) {
+        int index = 0;
+        while(index < bits.length - 1){
+            index += bits[index] + 1;
+        }
+        return index == bits.length - 1;
+    }
+}
