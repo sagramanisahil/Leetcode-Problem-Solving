dd0779d6e61b66c01d4f83f0c416016722ae0772
Sahil Kumar <sagramanisahil@gmail.com>
2024-10-09 14:18:35 +0500

    Add CountTreeNodes

---
+++ CountTreeNodes.java
@@ -0,0 +1,12 @@
+class CountTreeNodes {
+    public int countNodes(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+
+        int left = countNodes(root.left);
+        int right = countNodes(root.right);
+        
+        return left+right+1;
+    }
+}
