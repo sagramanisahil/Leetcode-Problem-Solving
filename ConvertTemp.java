504460eb7a6697860858b832c3fcad948659ef22
Sahil Kumar <sagramanisahil@gmail.com>
2024-10-21 13:04:12 +0500

    Add ConvertTemp

---
+++ ConvertTemp.java
@@ -0,0 +1,8 @@
+class ConvertTemp {
+    public double[] convertTemperature(double celsius) {
+        double kelvin = celsius + 273.15;
+        double Fahrenheit = celsius * 1.80 + 32.00;
+
+        return new double[]{kelvin,Fahrenheit};
+    }
+}
