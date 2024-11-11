27487079c7a3bb7379232fae110e154dc1bb857b
Sahil Kumar <sagramanisahil@gmail.com>
2024-11-11 10:33:04 +0500

    Add OddEvenLinkedList

---
+++ OddEvenLinkedList.java
@@ -0,0 +1,23 @@
+class OddEvenLinkedList {
+    public ListNode oddEvenList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode evenHead = head.next;
+        ListNode oddTail = head;
+        ListNode evenTail = evenHead;
+
+        while (evenTail != null && evenTail.next != null) {
+            oddTail.next = evenTail.next;
+            oddTail = oddTail.next;
+
+            evenTail.next = oddTail.next;
+            evenTail = evenTail.next;
+        }
+
+        oddTail.next = evenHead;
+
+        return head;
+    }
+}
