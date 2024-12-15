6e9026a1b2daaac47d11f8fb57ba87d946b9bfc5
Sahil Kumar <sagramanisahil@gmail.com>
2024-12-15 20:56:54 +0500

    Add FindMinRotatedSortedArray

---
+++ FindMinRotatedSortedArray.java
@@ -0,0 +1,17 @@
+class FindMinRotatedSortedArray {
+    public int findMin(int[] nums) {
+        int s = 0;
+        int e = nums.length-1;
+
+        while(s < e){
+            int mid = s + (e - s)/2;
+            if(nums[mid] < nums[e]){
+                e = mid;
+            }
+            else{
+                s = mid + 1;
+            }
+        }
+        return nums[s];
+    }
+}
