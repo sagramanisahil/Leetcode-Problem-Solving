55a6b3fca62ff15666050f7f3e0ddc2bdd6f03e5
Sahil Kumar <sagramanisahil@gmail.com>
2024-12-23 18:44:44 +0500

    Add isBalanced

---
+++ isBalanced.java
@@ -0,0 +1,28 @@
+class isBalanced{
+    public boolean isBalanced(TreeNode root) {
+        if(root == null){
+            return true;
+        }
+        if(Height(root) == -1){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+    public int Height(TreeNode root){
+        if(root == null){
+            return 0;
+        }
+        int left = Height(root.left);
+        int right = Height(root.right);
+
+        if(left == -1 || right == -1){
+            return -1;
+        }
+        if(Math.abs(left - right) > 1){
+            return -1;
+        }
+        return Math.max(left, right) + 1;
+    }
+}
